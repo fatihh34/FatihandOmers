ab701d0dde50aba3c2f4b536b89d5c05db277d72
Omer Buyukergene <buyukergene@gotmail.com>
2023-04-03 12:49:19 +0300

    şampiyon Gs

--- src/main/java/YeniProjedeNeYapacagiz.java
+++ src/main/java/YeniProjedeNeYapacagiz.java
@@ -5,5 +5,12 @@ public class YeniProjedeNeYapacagiz {
         String enBuyuk ="Fenerbahce";
         System.out.println("enBuyuk = " + enBuyuk);
 
+        int gspuan= 63;
+        int fbpuan=54;
+        if (fbpuan>gspuan){
+            System.out.println("Fb büyük");
+        }else System.out.println("****Gs büyük***");
+
     }
+
 }
